2f1052c2b8194050ea6be403a819bc2479030365
Abhijitam01 <Abhijitam01@users.noreply.github.com>
2026-01-28 23:14:43 +0530

    adding the prisma

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "./packages/db/prisma/schema.prisma",
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
-});
