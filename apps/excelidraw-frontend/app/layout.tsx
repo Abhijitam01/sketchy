d3afc385babcd895936da28654002ad734db46a6
Abhijitam01 <Abhijitam01@users.noreply.github.com>
2026-04-16 04:37:25 +0530

    feat: add Vercel Analytics

--- apps/excelidraw-frontend/app/layout.tsx
+++ apps/excelidraw-frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google"
 import "./global.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { ThemeToggle } from "@/components/ThemeToggle"
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -23,6 +24,7 @@ export default function RootLayout({
           <ThemeToggle />
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
